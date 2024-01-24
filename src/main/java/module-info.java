471882b44286bc5aeb1f33a6ddba9ebc69e5f602
wr0belj <jakub@wrobelj.com>
2024-01-24 18:27:43 +0100

    remove unnecessary exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,15 +7,9 @@ module com.example.psswd {
     requires org.xerial.sqlitejdbc;
     exports com.example.psswd.config to com.fasterxml.jackson.databind;
     opens com.example.psswd.config to com.fasterxml.jackson.databind;
-
-
     opens com.example.psswd.model to javafx.base;
-
     opens com.example.psswd to javafx.fxml;
-    exports com.example.psswd;
-    exports com.example.psswd.model;
-    exports com.example.psswd.views;
     opens com.example.psswd.views to javafx.fxml;
-    exports com.example.psswd.crypto;
     opens com.example.psswd.crypto to javafx.fxml;
+    exports com.example.psswd;
 }
